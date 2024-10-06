340006de53ec33b89c267e79e5afef263254571f
GilCarvalhal <gilbertocarvalhalmatos@gmail.com>
2024-10-06 16:25:38 -0300

    alteração na model Usuario

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -8,16 +8,19 @@
             <tr>
                 <th>Nome</th>
                 <th>Idade</th>
+                <th>CPF</th>
             </tr>
         </thead>
         <tbody>
             <tr>
                 <td>Gil</td>
                 <td>30</td>
+                <td>12345678910</td>
             </tr>
             <tr>
                 <td>Juberto</td>
                 <td>21</td>
+                <td>01987654321</td>
             </tr>
         </tbody>
     </table>
