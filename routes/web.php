67e4c790024bac6b5fdb7f54dab832ffbccb04d4
GilCarvalhal <gilbertocarvalhalmatos@gmail.com>
2024-10-10 15:36:12 -0300

    Implementação de novas rotas de limpeza padrão

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,42 @@
 <?php
 
-use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\UsuarioController;
+
+use Illuminate\Support\Facades\{
+    Route,
+    Artisan,
+};
+
+// Limpa o cache de todo o sistema.
+Route::get('/clear-cache', function () {
+    Artisan::call('cache:clear');
+    return "Cache is cleared";
+});
+
+// Limpa o cache de rotas.
+Route::get('/route-clear', function () {
+    Artisan::call('route:clear');
+    return "Cache is cleared";
+});
+
+// Limpa o cache de configuração.
+Route::get('/config-clear', function () {
+    Artisan::call('config:clear');
+    return "Cache is cleared";
+});
+
+// Limpa o cache de views (arquivos Blade compilados).
+Route::get('/view-clear', function () {
+    Artisan::call('view:clear');
+    return "View is cleared";
+});
+
+/* Cria um link simbólico entre o diretório storage e o diretório público (public).
+Isso é necessário para que arquivos armazenados (como uploads) sejam acessíveis publicamente. */
+Route::get('/storage-link', function () {
+    Artisan::call('storage:link');
+    return "View is cleared";
+});
 
 
 // ROTAS PARA USUÁRIO
